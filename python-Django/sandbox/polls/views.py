47da3a3efbfeedb11a9dba4d19d2242d86c0eef5
Bruno <bro_coly@hotmail.fr>
2019-05-16 11:50:38 +0100

    polls and admin pwd

--- python-Django/sandbox/polls/views.py
+++ python-Django/sandbox/polls/views.py
@@ -3,6 +3,9 @@ from django.template import loader
 from .models import Question, Choice
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
+from django.views import generic
+
+#RENDERING FROM HTML
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
@@ -16,8 +19,31 @@ def detail(request, question_id):
     return render(request, 'polls/detail.html', {'question': question})
 
 def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})
+
+
+#GENERIC VIEW
+
+
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by('-pub_date')[:5]
+
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
+
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
