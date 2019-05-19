cab608cf77aaac812227a9e724c484d7a6a333dd
Bruno <bro_coly@hotmail.fr>
2019-05-19 17:23:42 +0100

    setting up the algorithm in the view

--- python-Django/sandbox/recommender/views.py
+++ python-Django/sandbox/recommender/views.py
@@ -5,6 +5,12 @@ from scipy.sparse.linalg import svds
 import pandas as pd
 import numpy as np
 
+from .models import Feature
+from .models import Item
+from .models import Review
+from .models import Customer
+
+
 # Create your views here.
 
 
@@ -12,4 +18,53 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+
+    #Initialising the tabular for the computation
+    user_id = []
+    rating = []
+    item_id = []
+    items = []
+    cols = []
+    feats = []
+
+    #Other parameters
+    item_clusters=4
+    top_results=5
+
+    #We fill the tabular for the reviews and build the dataframe
+    for r in Review.objects.all():
+        item_id.append(r.item.id)
+        user_id.append(r.customer.id)
+        rating.append(r.note)
+    ratings = pd.DataFrame({'user_id':user_id,'item_id':item_id,'rating':rating})
+
+    #We take the list of items ID
+    for i in Item.objects.all():
+        items.append(i.id)
+
+    #We take the name of the features
+    for f in Feature.objects.all():
+        cols.append(f.feature_text)
+
+    #We have to create a set of sets of features
+    for i in items:
+        featI = []
+        completeFeatI = []
+        for f in Item.objects.filter(id=i)[0].item_features.all():
+            featI.append(f.feature_text)
+        for c in cols:
+            if c in featI:
+                completeFeatI.append(1)
+            else:
+                completeFeatI.append(0)
+        feats.append(completeFeatI)
+
+
+    #We create the dataframe for the features
+    item_df = pd.DataFrame(feats,index=items,columns=cols)
+
+    my_recommender = hybrid_recommender(item_clusters,top_results)
+    my_recommender.fit(ratings,item_df)
+
+    print(my_recommender.predict([1,2]))
+    return HttpResponse()
